0762cc54d3a20b5b9af83acd184cbd96d438115b
NguyenTrong <nguyentrong08101997@gmail.com>
2018-05-22 12:35:53 +0700

    them footer trong client , sua lai layout them user

--- admin/view/themtaikhoan_view.php
+++ admin/view/themtaikhoan_view.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Thêm tài khoản</title>
 	<link rel="stylesheet" type="text/css" href="view/css/themtaikhoan_view.css">
+    <script language="JavaScript" src="view/js/themtaikhoan.js"></script>
 </head>
 <body>
 	<?= require_once "header_view.php" ?>
@@ -12,9 +13,12 @@
                 Thêm một tài khoản mới
             </div>
             <form action="?action=taikhoan_controller" method="post">
+                <b>Tên tài khoản</b><br/>
                 <input class="input" type="text" name="username" placeholder="Tài Khoản"><br>
+                <b>Mật khẩu</b><br/>
+                <input class="input" type="password" name="password" placeholder="Mật Khẩu"><br/>
+                <b>Nhập lại mật khẩu</b><br/>
                 <input class="input" type="password" name="password" placeholder="Nhập Lại Mật Khẩu"><br>
-                <input class="input" type="password" name="password" placeholder="Mật Khẩu">
                 <br>
                 <input id="them" type="submit" name="submit" value="Thêm">
             </form>
